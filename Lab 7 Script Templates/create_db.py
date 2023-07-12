79d518dd66cff5021a261790b0da81c16f2b6d3e
Meet2406 <133835211+Meet2406@users.noreply.github.com>
2023-07-12 05:53:39 -0700

    step 1

--- Lab 7 Script Templates/create_db.py
+++ Lab 7 Script Templates/create_db.py
@@ -2,11 +2,11 @@
 Description:
  Creates the people table in the Social Network database
  and populates it with 200 fake people.
-
 Usage:
  python create_db.py
 """
 import os
+import sqlite3
 
 # Determine the path of the database
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -19,14 +19,65 @@ def main():
 def create_people_table():
     """Creates the people table in the database"""
     # TODO: Create function body
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    create_ppl_tbl_query = """
+    CREATE TABLE IF NOT EXISTS people
+(
+id       INTEGER PRIMARY KEY,
+name     TEXT NOT NULL,
+email    TEXT NOT NULL,
+address  TEXT NOT NULL,
+city     TEXT NOT NULL,
+province TEXT NOT NULL,
+bio      TEXT,
+age      INTEGER,
+created_at DATETIME NOT NULL,
+updated_at DATETIME NOT NULL
+);
+"""
+    cur.execute(create_ppl_tbl_query)
+    con.commmit()
+    con.close
+
     # Hint: See example code in lab instructions entitled "Creating a Table"
     return
 
 def populate_people_table():
     """Populates the people table with 200 fake people"""
     # TODO: Create function body
+
     # Hint: See example code in lab instructions entitled "Inserting Data into a Table"
-    # Hint: See example code in lab instructions entitled "Working with Faker"
+    from datetime import datetime
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+    add_person_query = """
+INSERT INTO people
+(
+name,
+email,
+address,
+city,
+province,
+bio,
+age,
+created_at,
+updated_at
+)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+"""
+    new_person = ('Bob Loblaw',
+   'bob.loblaw@whatever.net',
+   '123 Fake St.',
+   'Fakesville',
+   'Fake Edward Island',
+   'Enjoys making funny sounds when talking.',
+    46,
+    datetime.now(),
+    datetime.now())
+    cur.execute(add_person_query, new_person)
+    con.commit()
+    con.close()
     return
 
 if __name__ == '__main__':
